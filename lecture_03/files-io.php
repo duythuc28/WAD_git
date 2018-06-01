4e06f19b9688935ef3e55a0464de3d4422b3f9e7
iOS Developer <duythuc28493@gmail.com>
2018-06-01 12:27:37 +1000

    Add new function file_put_contents to write entire file and file() read entire a file into index array

--- lecture_03/files-io.php
+++ lecture_03/files-io.php
@@ -22,7 +22,7 @@
 
     // read from file
     $fp = fopen("file.txt", "r");   // open file pointer in read mode
-    while( ! feof($fp) ){           // iterate file pointer if true
+    while( !feof($fp) ){           // iterate file pointer if true
         echo fgets($fp);            // display string (line) from file
     }
     fclose($fp);                    // close file pointer
@@ -30,5 +30,26 @@
     // remove file
     unlink("file.txt"); // remove file
 
+    // The file_put_contents() function writes an entire
+    // file or appends a text string to a file
+    // file_put_contents(filename, string[, options])
+    file_put_contents("file.txt", "Hello World APPENDED", FILE_APPEND);
 
+    // READ ENTIRE FILE
+    // The file() function reads the contents of a file into an
+    // indexed array. It automatically recognises whether the lines in a text file end in \n, \r, or \r\n
+
+    $january = "48, 42, 68\n"; $january .= "48, 42, 69\n"; 
+    $january .= "49, 42, 69\n"; $january .= "49, 42, 61\n";
+    $january .= "49, 42, 65\n"; $january .= "49, 42, 62\n"; 
+    $january .= "49, 42, 62\n"; 
+    file_put_contents("sfjanaverages.txt", $january);
+    $januaryTemps = file("sfjanaverages.txt");
+    for ($i=0; $i<count($januaryTemps); $i++) {
+        $curDay = explode(", ", $januaryTemps[$i]); 
+        echo "<p><strong>Day " . ($i + 1). "</strong><br/>";
+        echo "High: {$curDay[0]}<br />"; 
+        echo "Low: {$curDay[1]}<br />";
+        echo "Mean: {$curDay[2]}</p>";
+    }
 ?>
